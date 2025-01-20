c0b59137a2228e7a502481922c15ab3792f98b9b
joelbyler272 <joelbyler272@gmail.com>
2025-01-20 14:01:03 -0500

    Commit

--- pages/_app.js
+++ pages/_app.js
@@ -12,7 +12,7 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <title>A Little God Time</title>
         <meta name="description" content="A daily devotional platform" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="A Little Time With God.ico" />
         
         {/* Google Fonts */}
         <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;700&family=Lato:wght@300;400;700&display=swap" rel="stylesheet" />
