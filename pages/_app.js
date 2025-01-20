985d79784acc3101e560e22d262687b31def6e8c
joelbyler272 <joelbyler272@gmail.com>
2025-01-20 15:04:56 -0500

    Favicon

--- pages/_app.js
+++ pages/_app.js
@@ -5,8 +5,8 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" href="/favicon.ico" />
+        <link rel="icon" href="/a_little_Time_with_God.ico.ico" />
+        <link rel="apple-touch-icon" href="/a_little_Time_with_God.ico" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <Component {...pageProps} />
